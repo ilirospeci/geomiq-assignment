82258eb8b1b840da0e396159af4819032f5a18e2
ilirospeci <ilir@bbros.eu>
2024-01-04 17:16:39 +0100

    Move result table seeding logic to CustomSeeder

--- app/Http/Controllers/StringParserController.php
+++ app/Http/Controllers/StringParserController.php
@@ -6,5 +6,5 @@ use Illuminate\Http\Request;
 
 class StringProcessingController extends Controller
 {
-
+    //
 }
--- database/seeders/CustomSeeder.php
+++ database/seeders/CustomSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CustomSeeder extends Seeder
 {
@@ -18,5 +19,23 @@ class CustomSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(ProfilesTableSeeder::class);
         $this->call(ModelHasRolesTableSeeder::class);
+
+        DB::statement("
+            INSERT INTO result (id, name, role, email, company_name, registered_on, last_login)
+            SELECT
+                u.id,
+                u.name,
+                COALESCE(r.name, 'default_role') AS role,
+                u.email,
+                p.company_name,
+                p.created_at AS registered_on,
+                u.last_login
+            FROM
+                users u
+                LEFT JOIN model_has_roles mhr ON u.id = mhr.model_id AND mhr.model_type = 'App\\\\Models\\\\User'
+                LEFT JOIN roles r ON mhr.role_id = r.id
+                LEFT JOIN profiles p ON u.id = p.user_id
+            ORDER BY u.id DESC
+        ");
     }
 }
